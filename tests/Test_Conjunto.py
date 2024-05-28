8a51fde550efe7e878aa53a654c4754fc9dc2b4d
Huaccho y Lazo <72620878@continental.edu.pe>
2024-05-27 21:00:21 -0500

    (ROJO) Listas vacias, error en el resultado esperado

--- tests/Test_Conjunto.py
+++ tests/Test_Conjunto.py
@@ -5,10 +5,19 @@ class TestCalcularPromedioPonderado(unittest.TestCase):
     def test_promedio_ponderado(self):
         calcular = CalcularPromedioPonderado()
         #Entrada de datos
-        Datos = [10, 12, 14] 
-        Pesos = [3, 4, 2]
-        resultado = calcular.PromedioPonderado(Datos,Pesos)
+        datos = [10, 12, 14] 
+        pesos = [3, 4, 2]
+        resultado = calcular.PromedioPonderado(datos,pesos)
         self.assertAlmostEqual(resultado, 11.78, 2)
+    
+    def test_conjuntos_vacios(self):
+        calcular = CalcularPromedioPonderado()
+        datos = []
+        pesos = []
+        resultado = calcular.PromedioPonderado(datos, pesos)
+        self.assertAlmostEqual(resultado,0,2)
+        '''with self.assertRaises(ValueError):
+            calcular.PromedioPonderado(datos, pesos)'''
 
 if __name__=='__main__':
     unittest.main()
